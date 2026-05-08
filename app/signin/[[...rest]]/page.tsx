bf95d5fc98f6f0506077da9d85a5eaf6cf77cf32
Jeremias Guttmann <jereguttmann3@gmail.com>
2026-05-08 20:39:18 -0300

    solucion error en page.tsx 2

--- app/signin/[[...rest]]/page.tsx
+++ app/signin/[[...rest]]/page.tsx
@@ -6,7 +6,7 @@ export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-zinc-50 dark:bg-black">
       <div className="w-full max-w-md rounded-xl bg-white p-6 shadow-md dark:bg-zinc-900">
-        <SignIn routing="path" />
+        <SignIn routing="path" path="/signin" />
       </div>
     </div>
   );
